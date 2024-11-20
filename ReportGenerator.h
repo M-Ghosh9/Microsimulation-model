0a29a463f8d53146fc8897930ed3959a112f542b
Mahima Ghosh <99464067+M-Ghosh9@users.noreply.github.com>
2024-11-20 12:54:59 +0000

    Update ReportGenerator.h

--- ReportGenerator.h
+++ ReportGenerator.h
@@ -5,11 +5,14 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include <map>
 
 class ReportGenerator {
 public:
     static void generateSimulationReport(const std::vector<std::shared_ptr<Patient>>& patients, const std::string& filename);
     static void saveSyntheticPopulation(const std::vector<std::shared_ptr<Patient>>& patients, const std::string& filename);
+    static void generatePopulationMetricsReport(const std::map<std::string, double>& metrics, const std::string& filename);
 };
 
 #endif // REPORTGENERATOR_H
+
